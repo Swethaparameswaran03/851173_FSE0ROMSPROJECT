ad516f3d740f5aaea739168b69155092dbba35ea
Swethaparameswaran03 <110654291+Swethaparameswaran03@users.noreply.github.com>
2022-08-22 23:27:51 +0530

    Updated with session timeout error

--- return-order-angular/src/app/app.module.ts
+++ return-order-angular/src/app/app.module.ts
@@ -4,26 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './component/login/login.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule} from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { ProcessComponent } from './component/process/process.component';
-
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; 
+import { HttpClientModule} from '@angular/common/http'; 
+import { FormsModule } from '@angular/forms'; 
+import { ProcessComponent } from './component/process/process.component'; 
+import { BnNgIdleService } from 'bn-ng-idle'; 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     ProcessComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule,
-    HttpClientModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+  imports: [ 
+    BrowserModule, 
+    AppRoutingModule, 
+     NgbModule, 
+    HttpClientModule, 
+     FormsModule 
+  ], 
+  providers: [BnNgIdleService], 
+  bootstrap: [AppComponent] 
+}) 
+export class AppModule { } 
