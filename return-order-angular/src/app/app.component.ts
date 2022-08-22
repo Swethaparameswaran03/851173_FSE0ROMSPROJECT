036ada21b3dd7dd531bbd39730a5ada9e32488b0
Swethaparameswaran03 <110654291+Swethaparameswaran03@users.noreply.github.com>
2022-08-22 23:27:08 +0530

    Updated with sessiontimeout error

--- return-order-angular/src/app/app.component.ts
+++ return-order-angular/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'; 
+import { BnNgIdleService } from 'bn-ng-idle'; 
+@Component({ 
+  selector: 'app-root', 
+  templateUrl: './app.component.html', 
+  styleUrls: ['./app.component.css'] 
+}) 
+export class AppComponent { 
+  title = 'return-order-angular'; 
+  expirationTimeInSeconds=300;  // session will get expired after 5 mintues of inactivty 
+  constructor(private bnIdle: BnNgIdleService) {  
+    this.bnIdle.startWatching(this.expirationTimeInSeconds).subscribe((res: any) => { 
+      if(res) { 
+          console.log("session expired"); 
+          localStorage.removeItem("token") 
+            window.location.href='' 
+      } 
+    }) 
+  } 
+} 
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'return-order-angular';
-}
+ 
+
+ 
